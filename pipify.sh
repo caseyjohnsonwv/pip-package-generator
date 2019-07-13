7e37c621ead13b6368b7bd6daafa97921dcc967b
Casey Johnson <ctj0001@mix.wvu.edu>
2019-07-13 00:09:07 -0400

    license contains link to choosealicense

--- pipify.sh
+++ pipify.sh
@@ -7,11 +7,14 @@ PKG_NAME=${PWD##*/}
 
 mkdir $PKG_NAME
 mv *.py ./$PKG_NAME
-echo "" > ./$PKG_NAME/__init__.py
-echo "" > setup.py
-echo "" > LICENSE
 echo -e "twine\nsetuptools\nwheel" > requirements.txt
+echo "" > setup.py
+echo "" > ./$PKG_NAME/__init__.
 
 if [ ! -f "README.md" ]; then
   echo "" > README.md
+fi
+
+if [ ! -f "LICENSE" ]; then
+  echo "https://choosealicense.com/" > LICENSE
 fi
