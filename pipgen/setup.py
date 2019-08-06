b4137e10b956c99f707720eaa5b1af6af087463c
Casey Johnson <casey.johnson@ablevets.com>
2019-08-06 07:19:32 -0400

    removed need for parity between READMEs

--- pipgen/setup.py
+++ pipgen/setup.py
@@ -1,15 +1,12 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-  long_description = fh.read()
-
 setuptools.setup(
   name = "pipgen",
   version = "0.1.0",
   author = "Casey Johnson",
   author_email = "ctj0001@mix.wvu.edu",
   description = "A package for easily creating and distributing pip packages.",
-  long_description = long_description,
+  long_description = "https://github.com/caseyjohnsonwv/pip-package-generator",
   long_description_content_type = "text/markdown",
   url = "https://github.com/caseyjohnsonwv/pip-package-generator",
   packages = setuptools.find_packages(),
