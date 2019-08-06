e93bedd895421c53f29244e9e2ff4734d6fbc858
Casey Johnson <casey.johnson@ablevets.com>
2019-08-06 07:33:05 -0400

    renamed update.sh for clarity

--- pipgen/setup.py
+++ pipgen/setup.py
@@ -1,17 +1,20 @@
 import setuptools
 
+with open("../README.md") as readmefile:
+    long_description = readmefile.read()
+
 setuptools.setup(
   name = "pipgen",
   version = "0.1.0",
   author = "Casey Johnson",
   author_email = "ctj0001@mix.wvu.edu",
   description = "A package for easily creating and distributing pip packages.",
-  long_description = "https://github.com/caseyjohnsonwv/pip-package-generator",
+  long_description = long_description,
   long_description_content_type = "text/markdown",
   url = "https://github.com/caseyjohnsonwv/pip-package-generator",
   packages = setuptools.find_packages(),
   include_package_data = True,
-  scripts = ['./bin/pipify.sh', './bin/depipify.sh', './bin/update.sh'],
+  scripts = ['./bin/pipify.sh', './bin/depipify.sh', './bin/pipdistribute.sh', './templates/setup'],
   install_requires = [],
   classifiers = [
     "Programming Language :: Python :: 3",
